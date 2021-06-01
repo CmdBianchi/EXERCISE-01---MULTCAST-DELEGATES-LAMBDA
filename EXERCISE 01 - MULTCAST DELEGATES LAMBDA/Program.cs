e05f409062dad7d27627878ea1a7cecb573a50bb
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-01 18:19:17 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 01 - MULTCAST DELEGATES LAMBDA/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Services;
+namespace EXERCISE_01___MULTCAST_DELEGATES_LAMBDA {
+    delegate void BinaryNumericOperatiom(double n1, double n2);
+    class Program {
+        static void Main(string[] args) {
+
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperatiom op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            op.Invoke(a, b);
+            
+
+
+        }
+    }
+}
